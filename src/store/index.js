eccccf7e3c660545413af7244c822e8200f12b8c
Monsij Biswal <biswalmonsij@gmail.com>
2020-09-12 18:03:05 +0530

    Add persistent login

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  plugins: [createPersistedState({
+    storage: window.sessionStorage,
+  })],
+
   state: {
     user: {
       uid: '',
